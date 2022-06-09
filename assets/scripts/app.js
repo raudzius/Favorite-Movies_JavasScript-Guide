ff00d638ffb1c5aa447bf125d3d15587305ef719
Mykolas Raudžius <raudziusmykolas@gmail.com>
2022-06-09 10:25:58 +0300

    feat: user can create movie object, input clears after adding/canceling

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,6 +5,8 @@ const cancelAddMovieBtn = addMovieModal.querySelector('.btn--passive');
 const addAddMovieBtn = cancelAddMovieBtn.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
 
+const movies = [];
+
 function toggleBackdrop() {
   backdrop.classList.toggle('visible');
 }
@@ -14,15 +16,36 @@ function toggleMovieModal() {
   toggleBackdrop();
 }
 
+function clearMovieInputs() {
+  for (const input of userInputs) {
+    input.value = '';
+  }
+}
+
 startAddMovieBtn.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', toggleMovieModal);
-cancelAddMovieBtn.addEventListener('click', toggleMovieModal);
+cancelAddMovieBtn.addEventListener('click', () => {
+  toggleMovieModal();
+  clearMovieInputs();
+});
 addAddMovieBtn.addEventListener('click', () => {
   const titleValue = userInputs[0].value;
   const imageUrlValue = userInputs[1].value;
   const ratingValue = userInputs[2].value;
+
   if (!titleValue.trim() || !imageUrlValue.trim() || !ratingValue.trim() || +ratingValue < 1 || +ratingValue > 5) {
     alert('Please enter valid values (rating between 1 and 5).');
     return;
   }
+
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieModal();
+  clearMovieInputs();
 });
