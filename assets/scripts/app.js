bdfa951624e191969860e53d3667baee1fd1d439
Mykolas Raudžius <raudziusmykolas@gmail.com>
2022-06-09 03:16:52 +0300

    feat: backdrop behind movie modal + cancel modal functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,17 @@
 const addMovieModal = document.getElementById('add-modal');
 const startAddMovieBtn = document.querySelector('header button');
+const backdrop = document.getElementById('backdrop');
+const cancelAddMovieBtn = addMovieModal.querySelector('.btn--passive');
+
+function toggleBackdrop() {
+  backdrop.classList.toggle('visible');
+}
 
 function toggleMovieModal() {
   addMovieModal.classList.toggle('visible');
+  toggleBackdrop();
 }
 
 startAddMovieBtn.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', toggleMovieModal);
+cancelAddMovieBtn.addEventListener('click', toggleMovieModal);
