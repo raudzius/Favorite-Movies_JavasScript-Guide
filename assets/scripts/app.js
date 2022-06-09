a3e56f15621c092840ba0cb51c3c18451c753c35
Mykolas Raudžius <raudziusmykolas@gmail.com>
2022-06-09 03:24:43 +0300

    feat: fetching and validating user input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,8 @@ const addMovieModal = document.getElementById('add-modal');
 const startAddMovieBtn = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
 const cancelAddMovieBtn = addMovieModal.querySelector('.btn--passive');
+const addAddMovieBtn = cancelAddMovieBtn.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll('input');
 
 function toggleBackdrop() {
   backdrop.classList.toggle('visible');
@@ -15,3 +17,12 @@ function toggleMovieModal() {
 startAddMovieBtn.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', toggleMovieModal);
 cancelAddMovieBtn.addEventListener('click', toggleMovieModal);
+addAddMovieBtn.addEventListener('click', () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+  if (!titleValue.trim() || !imageUrlValue.trim() || !ratingValue.trim() || +ratingValue < 1 || +ratingValue > 5) {
+    alert('Please enter valid values (rating between 1 and 5).');
+    return;
+  }
+});
